6cacaf6e6ae20ff8a86d0626da7395219176402d
Haram-Mubashar <fa25-bse-227@cuilahore.edu.pk>
2026-03-13 02:11:20 +0500

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -3,6 +3,8 @@ import java.util.Random;
 public class Deck{
 
        Card cards[];
+       int index=0;     // For drawing card through sequence of indexes
+
        private String suits[]={"Spades","Clubs","Hearts","Diamonds"};
        private String ranks[]={"Ace","2","3","4","5","6","7","8","Jack","Queen","King"};
        
@@ -25,27 +27,25 @@ public class Deck{
 
     }
 
+     public void shuffle(){
 
+     Random random=new Random();
+     for(int i=0;i<10000;i++){
+         int tempIndex = random.nextInt(52);
+         Card temp=cards[0];
+         cards[0]=cards[tempIndex];
+         cards[tempIndex]=temp;
+
+      }
     
+     }
 
+ public Card draw() {
+    if (index < cards.length) {
+        return cards[index++];   // gives cards in a sequence  
+  }
+    return null;   
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 }
