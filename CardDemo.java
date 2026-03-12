3c5e9df247e9deefaf3469d5d2016d1a7414cd01
Haram-Mubashar <fa25-bse-227@cuilahore.edu.pk>
2026-03-13 02:09:48 +0500

    Update CardDemo.java

--- CardDemo.java
+++ CardDemo.java
@@ -1,31 +1,23 @@
-public class CardDemo{
+public class CardDemo {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.shuffle();
+        deck.displayDeck();
 
-      public static void main(String[] args){
+        Player[] players = {
+            new Player("Haram"),
+            new Player("Sara"),
+            new Player("Ayesha")
+        };
 
-     Deck deck=new Deck();
-     deck.displayDeck();
+        Game game1 = new Game(players, deck);
 
+        game1.playRound();
+        System.out.println(game1);
 
+        game1.roundWinner();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
+            
 
-
-
-}
